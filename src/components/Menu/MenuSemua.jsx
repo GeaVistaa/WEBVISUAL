cc335bec3aefc34610b68128a390b4a69626c1d0
Demitri-Ansssss <ansoricahgreges@gmail.com>
2024-07-09 20:37:08 +0700

    first commit

---
+++ src/components/Menu/MenuSemua.jsx
@@ -0,0 +1,6 @@
+const MenuSemua = [
+  { id: 1, path: 'semua katagori', text: 'Semua Katagori' },
+  { id: 2, path: 'kost', text: 'Kost' },
+];
+
+export default MenuSemua;
---
+++ src/data/DataBaru.jsx
@@ -0,0 +1,149 @@
+import kost1 from '../assets/gambar-kost&kontrakan/kost1.svg';
+import kost2 from '../assets/gambar-kost&kontrakan/kost2.svg';
+import kost3 from '../assets/gambar-kost&kontrakan/kost3.svg';
+import kost4 from '../assets/gambar-kost&kontrakan/kost4.svg';
+import kontrakan1 from '../assets/gambar-kost&kontrakan/kontrakan1.svg';
+import kontrakan2 from '../assets/gambar-kost&kontrakan/kontrakan2.svg';
+import kontrakan3 from '../assets/gambar-kost&kontrakan/kontrakan3.svg';
+import kontrakan4 from '../assets/gambar-kost&kontrakan/kontrakan4.svg';
+import kost10 from '../assets/gambar-kost&kontrakan/kost10.svg';
+import kost12 from '../assets/gambar-kost&kontrakan/kost12.svg';
+import kos2 from '../assets/gambar-kost&kontrakan/kos2.svg';
+import kos3 from '../assets/gambar-kost&kontrakan/kos3.svg';
+
+const SemDat = [
+  {
+    id: 1,
+    title: 'Kost Olive Paragon',
+    katagoriFas: 'putra',
+    katagori: 'kost',
+    kecamatan: 'Jakarta Pusat',
+    fasilitas: 'K.Mandi dalam . Ac . Meja . Kasur ',
+    gambar: kost1,
+    harga: 'Rp.700.000',
+    satuan: 'Bulan',
+  },
+  {
+    id: 2,
+    title: 'Gelong Residence 25 Tipe A Grogol',
+    katagoriFas: 'putra',
+    katagori: 'kost',
+    kecamatan: ' Jakarta Barat',
+    fasilitas: 'K.Mandi dalam . Ac . Meja . Kasur . TV',
+    gambar: kost2,
+    harga: 'Rp.2.100.000',
+    satuan: 'Bulan',
+  },
+  {
+    id: 3,
+    title: 'Kost Erwin Duren Pancoran',
+    katagoriFas: 'putri',
+    katagori: 'kost',
+    kecamatan: 'Jakarta Pusat',
+    fasilitas: 'K.Mandi dalam . Ac . Meja . Kasur ',
+    gambar: kost3,
+    harga: 'Rp.700.000',
+    satuan: 'Bulan',
+  },
+  {
+    id: 4,
+    title: 'Olive Paragon Mall Tipe A',
+    katagoriFas: 'putri',
+    katagori: 'kost',
+    kecamatan: 'Jakarta Pusat',
+    fasilitas: 'K.Mandi dalam . Ac . Meja . Kasur ',
+    gambar: kost4,
+    harga: 'Rp.700.000',
+    satuan: 'Bulan',
+  },
+  {
+    id: 5,
+    title: 'Kost Badriah',
+    katagoriFas: 'Putri',
+    katagori: 'kost',
+    kecamatan: 'Depok',
+    fasilitas: 'K.Mandi dalam . Ac . Meja . Kasur ',
+    gambar: kontrakan1,
+    harga: 'Rp850.000',
+    satuan: 'Bulan',
+  },
+  {
+    id: 6,
+    title: 'Gelong Residence 25 Tipe A Grogol',
+    katagoriFas: 'Campur',
+    katagori: 'Kost',
+    kecamatan: 'Kecamatan Lewiliang',
+    fasilitas: 'K.Mandi dalam . Ac . Meja . Kasur ',
+    gambar: kontrakan2,
+    harga: 'Rp2.100.000',
+    satuan: '(Bulan pertama)',
+  },
+  {
+    id: 7,
+    title: ' Duren Tiga Tipe A Pancoran',
+    katagoriFas: 'Putra',
+    katagori: 'Kost',
+    kecamatan: 'Jakarta Selatan',
+    fasilitas: 'K.Mandi dalam . Ac . Meja . Kasur ',
+    gambar: kontrakan3,
+    harga: 'Rp2.700.000',
+    satuan: 'Bulan',
+  },
+  {
+    id: 8,
+    title: 'Kost Mbok Jum',
+    katagoriFas: 'Campur',
+    katagori: 'Kost',
+    kecamatan: 'Cilandak',
+    fasilitas: 'K.Mandi dalam . Ac . Meja . Kasur ',
+    gambar: kontrakan4,
+    harga: 'Rp.700.000',
+    satuan: 'Bulan',
+  },
+  {
+    id: 9,
+    title: 'Kost Gelora',
+    katagori: 'Kost',
+    katagoriFas: 'Putri',
+    kecamatan: 'Solo',
+    fasilitas: 'K.Mandi dalam . Ac . Meja . Kasur ',
+    gambar: kost10,
+    harga: 'Rp.750.000',
+    satuan: 'Bulan',
+  },
+  {
+    id: 10,
+    title: 'Kost Hidayah',
+    katagori: 'Kost',
+    katagoriFas: 'Putra',
+    kecamatan: 'Jember',
+    fasilitas: 'K.Mandi dalam . Ac . Meja . Kasur ',
+    gambar: kost12,
+    harga: 'Rp.670.000',
+    satuan: 'Bulan',
+  },
+  {
+    id: 11,
+    title: 'Ahmad Yani Blok D',
+    katagori: 'Kost',
+    katagoriFas: 'Campur',
+    kecamatan: 'Yogyakarta',
+    fasilitas: 'K.Mandi dalam . Ac . Meja . Kasur ',
+    gambar: kos3,
+    harga: 'Rp.950.000',
+    satuan: 'Bulan',
+  },
+  {
+    id: 12,
+    title: 'Pemuda Bangsa Blook B',
+    katagoriFas: 'Putri',
+    katagori: 'Kost',
+    kecamatan: 'Surabaya',
+    fasilitas: 'K.Mandi dalam . Ac . Meja . Kasur ',
+    gambar: kos2,
+    harga: 'Rp.800.000',
+    satuan: 'Bulan',
+  },
+];
+
+export default SemDat;
